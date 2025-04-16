ff5d828dcfe9bc383bedd04b17bf312953a4c3e4
Sasindu Gunawardana <sasindudananjaya2000@gmail.com>
2025-04-17 01:32:43 +0530

    feat: node product crud backend

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+
+//load environment variables 
+dotenv.config();
+
+//connect to database
+connectDB();
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
+
+//root route
+app.get('/', (req, res) => {
+  res.send(`API is runningg on port ${process.env.PORT}...\n without any errors`);
+});
+
+//error handling middleware 
+app.use(notFound);
+app.use(errorHandler);
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server runninnng in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
